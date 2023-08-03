a38f0d6f9fbd727ff0dc09bed4d957fec36f7c47
MOZGIII <mike-n@narod.ru>
2023-08-03 11:58:07 -0300

    Add testnet5 deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {
+  HttpNetworkAccountsUserConfig,
+  HttpNetworkUserConfig,
+} from "hardhat/types";
+
+const accounts = (prefix: string): HttpNetworkAccountsUserConfig => {
+  const privkey = process.env[`${prefix}_PRIVKEY`];
+  if (privkey) {
+    return [privkey];
+  }
+  return {
+    mnemonic:
+      process.env[`${prefix}_MNEMONIC`] ||
+      "test test test test test test test test test test test junk",
+  };
+};
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+  networks: {
+    // https://link.humanode.io/docs/chains
+    testnet5: {
+      chainId: 0x3a05,
+      url: "https://explorer-rpc-http.testnet5.stages.humanode.io",
+      accounts: accounts("TESTNET5"),
+    } satisfies HttpNetworkUserConfig,
+  },
 };
 
 export default config;
