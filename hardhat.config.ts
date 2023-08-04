22ca7f66fa51cbd273636eee3c46fcb3a2388e2c
MOZGIII <mike-n@narod.ru>
2023-08-04 00:23:44 -0300

    Add proper support for deploying to local humanode-peer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,10 +17,24 @@ const accounts = (prefix: string): HttpNetworkAccountsUserConfig => {
   };
 };
 
+const SUBSTRATE_DEV_SEED_PHRASE =
+  "bottom drive obey lake curtain smoke basket hold race lonely fit walk";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
 
   networks: {
+    "hmnd-local": {
+      url: "http://localhost:9933",
+      // Dev seed private keys.
+      accounts: {
+        mnemonic: SUBSTRATE_DEV_SEED_PHRASE,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 2,
+      },
+    },
+
     // https://link.humanode.io/docs/chains
     testnet5: {
       chainId: 0x3a05,
